a494a81510cf1b3f354af4b4d707a3c59141c860
Nick Strauch <njs35@duke.edu>
2024-07-21 17:23:27 -0400

    Updated to langchain

--- src/backend/llamafile.py
+++ src/backend/llamafile.py
@@ -1,18 +1,17 @@
-from openai import OpenAI
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
 
 
-client = OpenAI(
-    base_url= "http://127.0.0.1:8080/v1",
-    api_key="sk-none"
+model = ChatOpenAI(model="LLaMA_CPP", base_url="http://127.0.0.1:8080/v1", temperature=0.5)
+parser = StrOutputParser()
+system_template = "You are an expert data generator. You are the best at creating unique, realistic data samples."
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
 )
 
-completion = client.chat.completions.create(
-    model="LLaMA_CPP",
-    messages=[
-        {"role": "system", "content": "You are a great language model. You answer questions and generate text based on the user's input."},
-        {"role": "user", "content": "Write a funny joke about nature."}
-    ],
-    temperature=0.5
-)
+chain = prompt_template | model | parser
 
-print(completion.choices[0].message)
+human_prompt = "Create a conversation between a call center technician and a customer who is having trouble with their internet connection."
+print(chain.invoke({"text": human_prompt}))
